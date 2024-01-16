9d169da9a0791f93be4e19c9ee140ef755eb48be
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-01-16 15:23:32 +0100

    [init]:error404 and minor fix

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container mt-5">
+        <h1 class="text-center text-title">Errore 404</h1>
+    </div>
+@endsection
+
+@section('title')
+    | Homepage
+@endsection
