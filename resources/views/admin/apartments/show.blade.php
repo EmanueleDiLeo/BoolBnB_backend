98c4057646fee3cda0611122d25a7db515b513c8
EmanueleDiLeo <dileoemanuele11@gmail.com>
2024-01-16 10:26:23 +0100

    show-plus-1

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,47 +3,56 @@
 @section('content')
 <div class="container">
     <div class="card p-2 ">
-            @if (Str::contains($apartment->img, ['https://']))
-            <img src="{{ $apartment->img }}" alt="{{ $apartment->title }}">
-            @else
-                <img src="{{ asset('storage/' . $apartment->img) }}" alt="{{ $apartment->title }}">
-            @endif
-            <h4>{{ $apartment->title }}</h4>
-            <p>Indirizzo: {{ $apartment->address }}</p>
-            <p>lat: {{$apartment->lat}}</p>
-            <p>lon: {{$apartment->lon}}</p>
 
-            <h4>Info appartamento</h4>
-            <p>Stanze: {{ $apartment->room_number }}</p>
-            <p>Posti letto: {{ $apartment->bed_number }}</p>
-            <p>Bagni: {{ $apartment->bathroom_number }}</p>
-            <p>MQ: {{ $apartment->sq_metres }}</p>
+                @if (Str::contains($apartment->img, ['https://']))
+                    <img src="{{ $apartment->img }}" alt="{{ $apartment->title }}">
+                @else
+                    <img src="{{ asset('storage/' . $apartment->img) }}" alt="{{ $apartment->title }}">
+                @endif
 
-            <h4>Stato</h4>
-            @if ($apartment->visible === 1)
-            <p>Pubblico</p>
-            @else
-            <p>Privato</p>
-            @endif
+            <div class="card-body">
+                <h4>{{ $apartment->title }}</h4>
+                <p>Indirizzo: {{ $apartment->address }}</p>
+                <p>lat: {{$apartment->lat}}</p>
+                <p>lon: {{$apartment->lon}}</p>
 
-            <h4>Servizi</h4>
-            <ul>
-                @forelse ( $apartment->services as $service )
-                    <li>{{$service->name}}</li>
+                <h4>Info appartamento</h4>
+                <p>Stanze: {{ $apartment->room_number }}</p>
+                <p>Posti letto: {{ $apartment->bed_number }}</p>
+                <p>Bagni: {{ $apartment->bathroom_number }}</p>
+                <p>MQ: {{ $apartment->sq_metres }}</p>
+
+                <h4>Stato</h4>
+                @if ($apartment->visible === 1)
+                <p>Pubblico</p>
+                @else
+                <p>Privato</p>
+                @endif
+
+                <h4>Servizi</h4>
+                <ul>
+                    @forelse ( $apartment->services as $service )
+                        <li>{{$service->name}}</li>
+                    @empty
+                        <li>Nessun Servizio</li>
+                    @endforelse
+                </ul>
+
+                <h4>Descrizione</h4>
+                @if ($apartment->description)
+                <p>{{$apartment->description}}</p>
+                @else
+                    <p>Nessuna descrizione</p>
+                @endif
+
+
+                <h4>I tuoi messaggi</h4>
+                @forelse ($apartment->messages as $message)
+                    <p>Inviato da: {{ $message->sender_email }} <br>{{ $message->text }}</p>
                 @empty
-                    <li>Nessun Servizio</li>
+                    <p>Non hai messaggi</p>
                 @endforelse
-            </ul>
-
-            <h4>Descrizione</h4>
-            <p>{{$apartment->description}}</p>
-
-            <h4>I tuoi messaggi</h4>
-            @forelse ($apartment->messages as $message)
-                <p>Inviato da: {{ $message->sender_email }} <br>{{ $message->text }}</p>
-            @empty
-                <p>Non hai messaggi</p>
-            @endforelse
+            </div>
 
         </div>
     </div>
