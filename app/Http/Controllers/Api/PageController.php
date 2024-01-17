6670514384ed85fe7aa7c5a5db147b9187051cf4
EmanueleDiLeo <dileoemanuele11@gmail.com>
2024-01-17 19:13:57 +0100

    [FIX]: img api pagecontroller

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\Apartment;
 
@@ -11,12 +12,48 @@ class PageController extends Controller
     public function index()
     {
         $apartments = Apartment::where('visible', 1)->get();
+
+        foreach ($apartments as $apartment){
+            if($apartment) $success = true;
+            else $success = false;
+            if($success){
+                if($apartment->img){
+                    if(Str::contains($apartment->img, ['https://'])){
+
+                    }
+                    else{
+                        $apartment->img = asset('storage/' . $apartment->img);
+                    }
+                }else{
+                    $apartment->img = asset('storage/uploads/placeholder.webp');
+                }
+            }
+        }
+
         return response()->json($apartments);
     }
 
     public function searchApartments($tosearch)
     {
         $apartments = Apartment::where('title', 'LIKE', '%' . $tosearch . '%')->with('services')->get();
+
+        foreach ($apartments as $apartment){
+            if($apartment) $success = true;
+            else $success = false;
+            if($success){
+                if($apartment->img){
+                    if(Str::contains($apartment->img, ['https://'])){
+
+                    }
+                    else{
+                        $apartment->img = asset('storage/' . $apartment->img);
+                    }
+                }else{
+                    $apartment->img = asset('storage/uploads/placeholder.webp');
+                }
+            }
+        }
+
         return response()->json($apartments);
     }
 
