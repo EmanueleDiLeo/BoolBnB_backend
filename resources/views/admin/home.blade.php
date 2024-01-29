1bc055745bb96f824c412b97b9bc346eaec9e03f
Asiaviscardi <asia.viscardii@gmail.com>
2024-01-29 17:45:56 +0100

    [ADD] style home admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,19 +1,155 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1 class="text-center w-100 text-title">Home Admin</h1>
-    <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-        </h2>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card d-flex align-content-center">
-                    <h1 class="text-center my-5 log-ok">Log in Effettuato</h1>
+
+    <div class="landscape">
+        <h1 class="welcome">HOME ADMIN</h1>
+        <div class="mountain"></div>
+        <div class="mountain mountain-2"></div>
+        <div class="mountain mountain-3"></div>
+        <div class="sun-container sun-container-1">
+        </div>
+        <div class="sun-container">
+          <div class="sun"></div>
+        </div>
+        <div class="cloud"></div>
+        <div class="cloud cloud-1"></div>
+        <div class="sun-container sun-container-reflection">
+          <div class="sun"></div>
+        </div>
+        <div class="light"></div>
+        <div class="light light-1"></div>
+        <div class="light light-2"></div>
+        <div class="light light-3"></div>
+        <div class="light light-4"></div>
+        <div class="light light-5"></div>
+        <div class="light light-6"></div>
+        <div class="light light-7"></div>
+        <div class="water"></div>
+        <div class="splash"></div>
+        <div class="splash delay-1"></div>
+        <div class="splash delay-2"></div>
+        <div class="splash splash-4 delay-2"></div>
+        <div class="splash splash-4 delay-3"></div>
+        <div class="splash splash-4 delay-4"></div>
+        <div class="splash splash-stone delay-3"></div>
+        <div class="splash splash-stone splash-4"></div>
+        <div class="splash splash-stone splash-5"></div>
+        <div class="lotus lotus-1"></div>
+        <div class="lotus lotus-2"></div>
+        <div class="lotus lotus-3"></div>
+        <div class="front">
+          <div class="stone"></div>
+          <div class="grass"></div>
+          <div class="grass grass-1"></div>
+          <div class="grass grass-2"></div>
+          <div class="reed"></div>
+          <div class="reed reed-1"></div>
+        </div>
+    </div>
+
+    <div class="wrapper">
+        <div class="weather">
+          <div class="svg">
+            <svg version="1.1" id="rainclouds" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+               width="297px" height="308.5px" viewBox="0 0 297 308.5" enable-background="new 0 0 297 308.5" xml:space="preserve">
+              <g class="weather__raindrops">
+                <g class="raindrops--right">
+              <path class="weather__raindrops--1" d="M165.5,171.4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.3-19.2,16.8c-2.4,4.6-0.6,10.2,4,12.5
+                C157.6,177.7,163.2,176,165.5,171.4L165.5,171.4z M165.5,171.4"/>
+              <path class="weather__raindrops--2" d="M200,171.4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.3-19.1,16.8c-2.4,4.6-0.6,10.2,4,12.5
+                C192.1,177.7,197.7,176,200,171.4L200,171.4z M200,171.4"/>
+              <path class="weather__raindrops--3" d="M224.6,175.4c4.6,2.4,10.2,0.6,12.5-4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.3-19.2,16.8
+                C218.3,167.4,220.1,173,224.6,175.4L224.6,175.4z M224.6,175.4"/>
+              <path class="weather__raindrops--4" d="M161,209.9c4.6,2.4,10.2,0.6,12.5-4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.2-19.2,16.8
+                C154.6,201.9,156.4,207.5,161,209.9L161,209.9z M161,209.9"/>
+              <path class="weather__raindrops--5" d="M195.5,209.9c4.6,2.4,10.2,0.6,12.5-4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.2-19.2,16.8
+                C189.1,201.9,190.9,207.5,195.5,209.9L195.5,209.9z M195.5,209.9"/>
+            </g>
+                <g class="raindrops--left">
+              <path class="weather__raindrops--6" d="M68.5,205.9c2.4-4.6,2.6-25.3,2.6-25.3S54.4,192.8,52,197.3c-2.4,4.6-0.6,10.2,4,12.5
+                C60.6,212.2,66.2,210.4,68.5,205.9L68.5,205.9z M68.5,205.9"/>
+              <path class="weather__raindrops--7" d="M103,205.9c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.3-19.1,16.8c-2.4,4.6-0.6,10.2,4,12.5
+                C95.1,212.2,100.7,210.4,103,205.9L103,205.9z M103,205.9"/>
+              <path class="weather__raindrops--8" d="M127.6,209.9c4.6,2.4,10.2,0.6,12.5-4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.3-19.2,16.8
+                C121.3,201.9,123.1,207.5,127.6,209.9L127.6,209.9z M127.6,209.9"/>
+              <path class="weather__raindrops--9" d="M64,244.3c4.6,2.4,10.2,0.6,12.5-4c2.4-4.6,2.6-25.3,2.6-25.3S62.4,227.2,60,231.8
+                C57.6,236.4,59.4,242,64,244.3L64,244.3z M64,244.3"/>
+              <path class="weather__raindrops--10" d="M98.5,244.3c4.6,2.4,10.2,0.6,12.5-4c2.4-4.6,2.6-25.3,2.6-25.3s-16.8,12.2-19.2,16.8
+                C92.1,236.4,93.9,242,98.5,244.3L98.5,244.3z M98.5,244.3"/>
+            </g>
+              </g>
+              <g class="weather__clouds">
+
+                  <path d="M142.5,127.8c0,0-31.4-6.7-31.4-36.7
+                    s26.7-39.9,36.5-39.9s13.3,1.6,13.3,1.6s4.4-36.9,41.2-36.9s41.5,36.4,41.4,39.8c0,3.4,0.2,4.8-0.6,9.2c0,0,5.2-1.7,12.6-1.7
+                    c10.8,0,30.1,8,30.1,34s-30.4,30.6-30.4,30.6H142.5z"/>
+                  <path d="M46.2,164.7c0,0-31.4-6.7-31.4-36.7
+                    s26.7-39.9,36.5-39.9s13.3,1.6,13.3,1.6s4.4-36.9,41.2-36.9s41.5,36.4,41.4,39.8c0,3.4,0.2,4.8-0.6,9.2c0,0,5.2-1.7,12.6-1.7
+                    c10.8,0,30.1,8,30.1,34s-30.4,30.6-30.4,30.6H46.2z"/>
+
+              </g>
+              <g class="weather__lightning">
+                <polygon points="125.436,171.75 111.1,195.781 133.401,190.239 111.1,235.75 145.6,180.6 126.35,185.6 138,171.75
+                  "/>
+                <polygon points="207.686,144.42 193.35,168.451 215.651,162.909 193.35,208.42 227.85,153.27 208.6,158.27
+                  220.249,144.42 "/>
+              </g>
+            </svg>
+
+          </div>
+          <span class="weather__date">New York, NY &#8212; <span id="date"></span></span>
+          <h2 class="weather__pretitle">It is Raining.</h2>
+          <h1 class="weather__title">You will get wet.</h1>
+        </div>
+
+
+
+        <div class="row d-flex align-items-center ">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Appartamenti</h5>
+                  <p class="card-text">Tieni aggiornati i tuoi appartamenti, attirerà piu clienti!</p>
+                  <a href="#" class="btn btn-primary">I tuoi appartamenti</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Appartamenti</h5>
+                  <p class="card-text">Se hai piu appartamenti puoi aggiungerli con un click</p>
+                  <a href="#" class="btn btn-primary">Aggiungi appartamento</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Messaggi</h5>
+                    <p class="card-text">Tieni d'occhio la sezione messaggi, tenersi in contatto con i propri clienti è importante</p>
+                    <a href="#" class="btn btn-primary">Visualizza messaggi</a>
+                  </div>
+                </div>
+            </div>
+
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Sponsorizzazioni</h5>
+                    <p class="card-text">Sponsorizza ora i tuoi appartamenti, i clienti li visualizzeranno come primi risultati</p>
+                    <a href="#" class="btn btn-primary">Sponsorizza</a>
+                  </div>
                 </div>
             </div>
         </div>
     </div>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id vel molestiae cupiditate, quam quasi blanditiis voluptatem autem possimus voluptate sapiente quisquam accusamus deleniti debitis? Perferendis, repellat totam inventore omnis distinctio harum quaerat ratione reprehenderit quo doloribus voluptate aperiam veritatis aliquid id nam voluptatibus? Perspiciatis ea consectetur cum vel, dolore libero architecto corporis suscipit quaerat sunt? Unde dicta assumenda repellat? Eveniet minus repellendus autem quibusdam, alias saepe repudiandae vero accusantium, quaerat labore, neque error quisquam natus aspernatur officia est consectetur! Id perferendis ab error quaerat ea quam sed sint animi laboriosam? Excepturi velit corrupti adipisci aliquid. Iure, possimus fuga! Tenetur recusandae eaque quaerat doloremque sunt incidunt culpa accusamus quisquam, dignissimos neque porro cum consectetur minima repellat nihil consequuntur esse reprehenderit saepe! A eaque suscipit totam ducimus vero laudantium ullam culpa, ut obcaecati. Ratione ex ullam obcaecati ipsum magnam enim dolores rerum? Vero debitis vitae rem assumenda nostrum, rerum eligendi, enim nobis magnam, dignissimos aspernatur sit aperiam exercitationem delectus nulla veniam recusandae perspiciatis ad cupiditate laborum magni? Ipsa sunt magni qui dicta animi eum adipisci tempora pariatur placeat laborum optio officia asperiores obcaecati maiores, recusandae voluptates, id omnis esse delectus quaerat repellendus fugit? Animi cum velit, laudantium praesentium repudiandae, dolores fuga quod quis tenetur ducimus, assumenda suscipit voluptas blanditiis ipsum reiciendis soluta veniam unde commodi sapiente inventore reprehenderit error tempore? Enim vitae dicta rerum impedit dolor ipsum esse? Sed praesentium consequatur consectetur soluta? Eligendi nobis animi atque deserunt molestiae perspiciatis quod consequatur sint asperiores totam consequuntur hic ab, dolores eum nihil veniam id? Neque ullam praesentium, reprehenderit quidem sunt cumque laborum necessitatibus hic quibusdam earum error possimus illo magnam quaerat asperiores exercitationem. Rerum molestiae laudantium ab dignissimos voluptatibus! Assumenda dolorem quibusdam maxime hic fuga mollitia tempore nulla esse perferendis sit. Dicta assumenda quod autem minima neque inventore, at natus ratione deserunt, animi quia reiciendis. Labore praesentium voluptates repellat et, saepe facilis nulla dicta repellendus eligendi, maiores ducimus nam aperiam provident exercitationem. Itaque nobis aut minima sequi repudiandae vel veritatis molestias error, quasi eligendi quis culpa placeat natus velit fugit harum dolorem, consectetur, nam pariatur ipsum corporis aperiam exercitationem. Harum sint repellendus consequuntur consequatur, non a quam cumque ad repudiandae. Maxime expedita quae ab minima numquam, rerum id facere animi mollitia corrupti repellendus eligendi, eum assumenda debitis necessitatibus in quasi omnis aliquam beatae minus ut aliquid tempora dolore optio. Optio quae est ut voluptatum voluptas quasi natus vel nihil. Obcaecati adipisci, modi dignissimos eveniet quo explicabo quis voluptatibus ullam asperiores sint iste. Dolores esse ea, vero iusto nostrum ipsum facilis suscipit repellendus odit eligendi. Nam excepturi doloremque soluta repudiandae asperiores aliquam! Modi ad tenetur perspiciatis mollitia a accusamus autem repellendus repudiandae, sunt fuga? Mollitia officia cupiditate cum perferendis nobis voluptatem eligendi quo et inventore voluptas id, eveniet harum illum exercitationem ducimus ipsa nihil sunt assumenda. Aspernatur inventore veniam cum eos eius? Magni laborum pariatur, est voluptatum, assumenda odit ullam atque explicabo tempore commodi optio? Quis repellat, eligendi magnam dolore molestias eum facere modi incidunt fugit odit obcaecati placeat ipsum adipisci. Corporis, nemo nihil repellendus voluptate cumque natus dicta provident autem laboriosam labore. Ratione quis odio nulla maxime optio vero, molestiae assumenda hic aliquam distinctio, accusantium doloribus architecto ut consequatur exercitationem itaque expedita eveniet culpa obcaecati praesentium tempore ab aspernatur! Dicta earum fuga eos repellat numquam eveniet explicabo sequi quam, omnis, culpa enim? Ducimus laborum possimus corporis sunt iste, beatae obcaecati! Quis possimus vel quae officia similique ipsam natus, iste architecto commodi nihil voluptate, sit ex. Illo eos officiis a similique voluptatibus veniam aperiam quia. Pariatur quas consequuntur fugit doloribus porro quam enim facere, repellat fugiat dolorem temporibus perspiciatis maiores! Maiores suscipit, iure aperiam laborum numquam placeat dolorum illo quis reiciendis hic. Numquam quos aliquid facere voluptatibus aliquam natus debitis culpa repellat? Perspiciatis, molestias itaque est corrupti sapiente esse omnis sequi modi, impedit illum delectus placeat veritatis provident? Molestias corrupti, maiores optio eos consectetur in saepe iure, ipsam facilis sequi consequuntur omnis magni voluptatibus quia rerum beatae minima voluptatem. Ullam provident accusantium fugit, ipsam obcaecati esse eos laboriosam doloribus quaerat ea id culpa, excepturi porro recusandae adipisci ratione itaque explicabo, laudantium quo odit! Distinctio nostrum reiciendis placeat, illum nobis nemo dignissimos autem quo ea ratione nesciunt, nam laudantium obcaecati voluptates omnis suscipit expedita consequuntur consectetur. Recusandae illum perferendis earum culpa doloribus, saepe molestias maxime cupiditate veniam deserunt sequi iusto dignissimos nisi at ipsum aut provident cum error. Nemo, libero. Nihil nesciunt adipisci dolor saepe. Temporibus odio error dignissimos est vitae minima amet aliquam obcaecati nobis adipisci tempora perspiciatis, nam nulla, commodi provident nihil. Omnis nobis modi reprehenderit saepe. Sint facere dolorem ducimus. Atque officia obcaecati facilis porro magnam, minima minus ex voluptatum non omnis nam sit at, accusamus quam inventore incidunt repudiandae, culpa tempore dolorum numquam error fugiat? Sint ad, eaque odit architecto aliquam repellat delectus quos error, provident veritatis deserunt aliquid molestiae atque vel illo unde sed, ut illum! Ab dignissimos ipsum natus ex iure voluptas optio eos laboriosam, distinctio perspiciatis similique quo culpa autem aliquid. Maxime, pariatur cupiditate vitae delectus maiores aspernatur unde suscipit alias cum praesentium accusantium, nobis reiciendis. Corrupti magnam, iure, reprehenderit quod unde fugiat quos eaque natus voluptate ducimus blanditiis totam aperiam aspernatur adipisci consectetur aliquam illo iste nisi quia qui esse, temporibus vero? Accusantium deleniti soluta quos error sequi sint quidem ullam, in voluptatum doloribus, reiciendis quam eum placeat doloremque nostrum assumenda animi vel dolorum ab praesentium nesciunt. Iure ducimus eum maxime deleniti magnam optio tenetur mollitia qui sapiente, dolor veritatis! Harum ullam ratione et iusto numquam totam neque accusamus voluptas labore repudiandae amet possimus consequatur quos ipsam iure, illum dolorum impedit aut. Optio, nihil eaque. Magni ipsa reiciendis laboriosam praesentium in consectetur culpa beatae perspiciatis provident ullam cupiditate aliquam excepturi veritatis, quod amet minima, itaque necessitatibus sequi dolorem? Corporis voluptatibus reiciendis consequatur minima, mollitia adipisci eius illum unde totam possimus sed iusto culpa laudantium dignissimos facere saepe fuga molestiae ipsum architecto ea a quae ipsa ut facilis? Facilis quos sunt quas et accusamus distinctio, necessitatibus repudiandae! Dolore ipsa dolor optio eligendi. Labore perspiciatis numquam explicabo magni est.</p>
+
+
+
+
+
 @endsection
 
 @section('title')
