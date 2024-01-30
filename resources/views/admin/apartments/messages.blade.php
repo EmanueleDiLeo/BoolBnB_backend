d2187d907aab2a48edfb5703c4a08f69d8120e26
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-01-31 00:13:08 +0100

    [add]:responsive form message aside accordion

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -9,22 +9,26 @@
         @endif
 
         @forelse ($messages as $message)
-        <div class="accordion accordion-flush email-box " id="accordionFlushExample{{$message->id}}">
-            <div class="accordion-item">
-              <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{$message->id}}" aria-expanded="false" aria-controls="flush-collapseOne">
-                    <strong class="me-2">Messaggio dell'appartamento:</strong> {{ $message->title}}
-                </button>
-              </h2>
-              <div id="flush-collapse{{$message->id}}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                    <span class="text-secondary">Inviato da: {{ $message->sender_email }}</span> <br />
-                    <span class="text-secondary">Inviato il: {{Helper::formatDate($message->sended_at)}}</span>
-                    <p>{{ $message->text }}</p>
+            <div class="accordion accordion-flush email-box " id="accordionFlushExample{{ $message->id }}">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed p-1 p-sm-3" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapse{{ $message->id }}" aria-expanded="false"
+                            aria-controls="flush-collapseOne"><i class="fa-solid fa-envelope text-primary me-2">
+                                <strong class="me-2 d-none d-md-block"></i>Messaggio dell'appartamento:</strong>
+                            {{ $message->title }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapse{{ $message->id }}" class="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            <span class="text-secondary">Inviato da: {{ $message->sender_email }}</span> <br />
+                            <span class="text-secondary">Inviato il: {{ Helper::formatDate($message->sended_at) }}</span>
+                            <p>{{ $message->text }}</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-        </div>
         @empty
             <h2>Non hai messaggi</h2>
         @endforelse
--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,11 +1,45 @@
-<aside class="aside-style h-100" >
-    <div class=" d-md-none text-white pt-4 px-4 fs-2">
-        <i id="burger-menu-icon" class="fa-solid fa-bars"></i>
+<aside class="aside-style h-100">
+
+    <div class="d-flex w-100 justify-content-center mt-3">
+        <a class="d-md-none text-white text-center fs-1" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
+            aria-controls="offcanvasExample">
+            <i id="burger-menu-icon" class="fa-solid fa-bars"></i>
+        </a>
+
     </div>
+
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul id="link-menu" class="text-start py-3 px-4  d-md-block">
+                <li class="my-3">
+                    <a href="{{ route('admin.home') }}" @class(['active' => Route::is('admin.home')])>
+                        Home</a>
+                </li>
+                <li class="my-3">
+                    <a href="{{ route('admin.apartments.index') }}" @class(['active' => Route::is('admin.apartments.index')])>I Tuoi Appartamenti
+                    </a>
+                </li>
+                <li class="my-3">
+                    <a href="{{ route('admin.apartments.create') }}" @class(['active' => Route::is('admin.apartments.create')])>Aggiungi Appartamento
+                    </a>
+                </li>
+                <li class="my-3">
+                    <a href="{{ route('admin.messages') }}" @class(['active' => Route::is('admin.messages')])>Messaggi Ricevuti
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
     <ul id="link-menu" class="text-start py-3 px-4 d-none d-md-block">
         <li class="my-3">
             <a href="{{ route('admin.home') }}" @class(['active' => Route::is('admin.home')])>
-            Home</a>
+                Home</a>
         </li>
         <li class="my-3">
             <a href="{{ route('admin.apartments.index') }}" @class(['active' => Route::is('admin.apartments.index')])>I Tuoi Appartamenti
@@ -21,14 +55,3 @@
         </li>
     </ul>
 </aside>
-
-<script>
-    let burgerMenu= document.getElementById('burger-menu-icon');
-    let linkMenu= document.getElementById('link-menu');
-    let aside= document.querySelector('.aside-style');
-
-    burgerMenu.addEventListener('click', function(){
-        linkMenu.classList.toggle("d-none");
-        aside.classList.toggle("w-50")
-    })
-</script>
