d6db36fd139afd6e3638a4acc3d74f3087495683
VikVanDik <95vlc17@gmail.com>
2024-01-31 12:39:17 +0100

    [add] else condition to order controller and aside width

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -9,7 +9,7 @@
     </div>
 
 
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas d-md-none offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
         <div class="offcanvas-header">
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
